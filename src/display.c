07107a911782e09673cd9d894641db71d003c27b
mapy42130 <yoann.picquenot@epitech.eu>
2015-05-23 18:46:08 +0200

    update display

--- src/display.c
+++ src/display.c
@@ -1,6 +1,13 @@
+#include <unistd.h>
 #include "parser.h"
 
-void	display()
+void	display_topic(t_topic *topic)
+{
+  printf("");
+  return ;
+}
+
+void	display_topic_content(t_topic_content *content)
 {
   return ;
 }
