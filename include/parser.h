55cc5e88ebc4b94ba64e75227c88208559737e1d
mapy42130 <yoann.picquenot@epitech.eu>
2015-05-23 18:49:30 +0200

    parser

--- include/parser.h
+++ include/parser.h
@@ -2,12 +2,19 @@
 # define PARSER_H_
 
 typedef struct		s_topic	t_topic;
+typedef struct		s_topic_content t_topic_content;
 
 struct			s_topic
 {
   char			*name;
   char			*id;
   t_topic		*next;
+};
+
+struct			s_topic_content
+{
+  char			*content;
+  t_topic_content	*next;
 };
 
 #endif /* !PARSER_H_ */
