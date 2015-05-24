be64cd8389f5a14d4f0267586ec9e15facb89004
cchardeau <corentin.chardeau@epitech.eu>
2015-05-24 15:53:04 +0200

    rm libcurl

---
+++ src/get.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+
+struct MemoryStruct {
+  char *memory;
+  size_t size;
+};
+
+static size_t	WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+  size_t realsize = size * nmemb;
+  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+  if (mem->memory == NULL)
+    {
+    printf("not enough memory (realloc returned NULL)\n");
+    return 0;
+  }
+ 
+  memcpy(&(mem->memory[mem->size]), contents, realsize);
+  mem->size += realsize;
+  mem->memory[mem->size] = 0;
+ 
+  return realsize;
+}
+
+int get()
+{
+  CURL *curl_handle;
+  CURLcode res;
+ 
+  struct MemoryStruct chunk;
+ 
+  chunk.memory = malloc(1); 
+  chunk.size = 0;
+
+
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  curl_handle = curl_easy_init();
+
+
+  curl_easy_setopt(curl_handle, CURLOPT_URL, "https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Forum_des_nouveaux/Flow");
+
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+
+  curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+  res = curl_easy_perform(curl_handle);
+
+  if (res != CURLE_OK) {
+    fprintf(stderr, "curl_easy_perform() failed: %s\n",
+            curl_easy_strerror(res));
+  }
+  else {
+    printf("%s\n", chunk.memory);
+  }
+
+  curl_easy_cleanup(curl_handle);
+
+  free(chunk.memory);
+
+  curl_global_cleanup();
+
+  return 0;
+}
