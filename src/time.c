41fa7d07c4d27cdc711b477059e88054f264bf71
cchardeau <corentin.chardeau@epitech.eu>
2015-05-25 15:30:19 +0200

    Add convert time function

--- src/time.c
+++ src/time.c
@@ -3,7 +3,14 @@
 
 const char	*timestampToLocalTime(const char *timestamp)
 {
-  if (!timestamp)
+  time_t	time;
+  char		*buffer;
+  struct tm	*loctime;
+
+  if (!timestamp || !(buffer = calloc(256, sizeof(char))))
     return (NULL);
-  return (NULL);
+  time = atoi(timestamp);
+  loctime = localtime(&time);
+  strftime(buffer, 256, "%D %R", loctime);
+  return (buffer);
 }
